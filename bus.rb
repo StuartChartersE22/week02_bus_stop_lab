6a9f0c2504238e4dc310f3b3380257379671c112
Iona Macbeth <ionalmacbeth@gmail.com>
2018-05-29 14:03:03 +0100

    passed bus has route num

---
+++ bus.rb
@@ -0,0 +1,11 @@
+class Bus
+
+  attr_reader(:route_num, :destination) 
+
+  def initialize(route_num, destination)
+    @route_num = route_num
+    @destination = destination
+    @passengers = []
+  end
+
+end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -6,9 +6,14 @@ require_relative("../person.rb")
 class BusTest < MiniTest::Test
 
   def setup
+    passenger1 = Person.new("Stuart", 26)
+    passenger2 = Person.new("Iona", 27)
 
-
+    @bus1 = Bus.new(22, "Ocean Terminal")
   end
 
+  def test_bus_has_route_number
+    assert_equal(22, @bus1.route_num())
+  end
 
 end
